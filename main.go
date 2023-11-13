f00da8260b09b43d10f229786d45f17447debde2
AbdelRahmanYaghi <abd.a.yaghi@gmail.com>
2023-11-13 21:20:28 +0300

    Dataframe Commit #1

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package gosv
 
-import "log"
+import (
+	"os"
+	"log"
+)
 
-func LogInfo(message string) {
-	log.Println(message)
+type DataFrame struct {
+	seperator string
+	filepath string
+	mat [][]float64
+}
+
+func (df *DataFrame) Read_csv(path string) {
+	// read_csv reads the csv file and returns a dataframe
+	file, err := os.Open(path)
+  	
+	if err != nil { log.Println("Failed Opening CSV file") }
+
+	defer file.Close()
+
+	log.Println("Successfully Opened CSV file")
 }
